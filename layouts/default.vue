fdb483ff07036b2e2daa446463c33fda74e32654
Promo69 <promo69@protonmail.com>
2022-05-04 22:05:08 -0500

    changed button order

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div class="nav">
+      <NuxtLink class="link" :to="{ name: 'index' }">Home</NuxtLink>
       <NuxtLink class="link" :to="{ name: 'about' }">About</NuxtLink>
       <NuxtLink class="link" :to="{ name: 'contact' }">Contact</NuxtLink>
-      <NuxtLink class="link" :to="{ name: 'index' }">Home</NuxtLink>
     </div>
-    <slot/>
+    <slot />
   </div>
 </template>
 <style scoped>
