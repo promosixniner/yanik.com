d4c060298edb24562bc9f3d40464af7c97b3ce96
Promo69 <promo69@protonmail.com>
2022-05-08 17:34:58 -0500

    allign index txt

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,15 @@
   <div>
     <Title>Hello this is Yanik Love's web space</Title>
 
-    <p>
+    <p class="maintext">
       On this page I will share with you some of my thoughts, art, projects and
       more. Feel free to contact me if you would to cooperate or connect in any
       way :)
     </p>
   </div>
 </template>
-
-
+<style>
+.maintext {
+  text-align: center;
+}
+</style>
